988c660ec359cc931272200c2a84f38a2c640f55
minhye9731 <jenney62639731@gmail.com>
2022-07-13 18:27:49 +0900

    이미지 데이터 적용방식 개선

--- EmotionalDiaryProject/ViewController.swift
+++ EmotionalDiaryProject/ViewController.swift
@@ -9,17 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var emotionDataArray: [Emotion] = [
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime1.png")!, emotions: "행복해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime2.png")!, emotions: "사랑해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime3.png")!, emotions: "좋아해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime4.png")!, emotions: "분노해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime5.png")!, emotions: "심심해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime6.png")!, emotions: "지루해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime7.png")!, emotions: "당황해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime8.png")!, emotions: "서운해"),
-        Emotion(emotionalSlime: UIImage(named: "sesac_slime9.png")!, emotions: "슬퍼해")
-    ]
+    var emotionDataArray: [String] = ["행복해", "사랑해", "좋아해", "분노해", "심심해", "지루해", "당황해", "서운해", "슬퍼해"]
     
     @IBOutlet var slimeImages: [UIButton]!
     @IBOutlet var emotionLabels: [UILabel]!
@@ -28,15 +18,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for i in 0...8 {
-            slimeImages[i].setImage(emotionDataArray[i].emotionalSlime, for: .normal)
-            emotionLabels[i].text = #"\#(emotionDataArray[i].emotions) "0""#
+        for i in 0...(emotionArray.count - 1) {
+            let image = UIImage(named: "sesac_slime\(i + 1).png")
+            slimeImages[i].setImage(image!, for: .normal)
+            emotionLabels[i].text = #"\#(emotionDataArray[i]) "0""#
         }
     }
     
     // MARK: - 감정버튼 클릭시 로직
     @IBAction func buttonTapped(_ sender: UIButton) {
         emotionArray[sender.tag] += 1 // 클릭한 버튼의 tag 찾고, 해당 서브스크립트 감정숫자에 1 더해줌
-        emotionLabels[sender.tag].text = #"\#(emotionDataArray[sender.tag].emotions) \#(emotionArray[sender.tag])"# // 전체 감정숫자 출력
+        emotionLabels[sender.tag].text = #"\#(emotionDataArray[sender.tag]) \#(emotionArray[sender.tag])"# // 전체 감정숫자 출력
     }
 }
